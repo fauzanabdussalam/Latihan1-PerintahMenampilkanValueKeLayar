32b36e76d5dedfb2e961e357bf5ead19b164e7f4
fauzan_abdussalam <fauzan@3trust.com>
2020-11-25 22:51:27 +0700

    Initial Commit

---
+++ src/pbo10k/pkg10119901/latihan1/perintahmenampilkanvaluekelayar/PBO10K10119901Latihan1PerintahMenampilkanValueKeLayar.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo10k.pkg10119901.latihan1.perintahmenampilkanvaluekelayar;
+
+/**
+ *
+ * @author
+ * NAMA     : Fauzan Muhammad Abdussalam
+ * KELAS    : IF-10K
+ * NIM      : 10119901
+ * Deskripsi Program : Menampilkan Value ke Layar
+ *
+ */
+public class PBO10K10119901Latihan1PerintahMenampilkanValueKeLayar {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("BIODATA ROCKSTAR" + "\n" + "-------------------------------------");
+        System.out.println("NAMA \t\t : Fauzan Muhammad Abdussalam");
+        System.out.println("ALAMAT \t\t : Sumedang");
+        System.out.println("TEMPAT LAHIR\t : Sumedang");
+        System.out.println("TANGGAL LAHIR \t : 07 April 1999");
+        System.out.println("---------------------------");
+        System.out.println("HOBI \t\t : Sepak Bola");
+        System.out.println("MAKES \t\t : Pizza");
+        System.out.println("MIKES \t\t : Kopo");
+    }
+    
+}
